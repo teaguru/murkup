ff53f7d16ff838c8d6c7ca57c99d13da927e0934
teaguru <teaguru@bk.ru>
2019-03-25 21:41:47 +0400

    насколько такое вообще законно?

--- js/modules/common/menu.js
+++ js/modules/common/menu.js
@@ -3,25 +3,29 @@
     var elem2 = document.querySelector(".logo-menu");
     var block = document.querySelectorAll(".header__item");
     var logo = document.querySelector(".logo");
+    var flag = true;
 //declaration of visibility
     for(var i in Object.keys(block))  (block[i].classList.add('no-menu'));
     elem.classList.add('no-menu');
-           elem2.onclick = function(){
+          logo.onclick = function(){
+
+                  if (flag == true)  {
         for(var i in Object.keys(block))  (block[i].classList.add('new-menu'));
             for(var i in Object.keys(block))  (block[i].classList.remove('no-menu'));
                elem2.classList.add('no-menu');
                elem2.classList.remove('new-menu');
                elem.classList.add('new-menu');
                elem.classList.remove('no-menu');
-
+               flag = false;
+               console.log(flag); }
+                  else {
+                      for(var i in Object.keys(block))  (block[i].classList.add('no-menu'));
+                  for(var i in Object.keys(block))  (block[i].classList.remove('new-menu'));
+                  elem.classList.add('no-menu');
+                  elem.classList.remove('new-menu');
+                  elem2.classList.add('new-menu');
+                  elem2.classList.remove('no-menu');
+                  flag = true;
+                  console.log(flag); }
     }
 
-    elem.onclick = function(){
-        for(var i in Object.keys(block))  (block[i].classList.add('no-menu'));
-        for(var i in Object.keys(block))  (block[i].classList.remove('new-menu'));
-        elem.classList.add('no-menu');
-        elem.classList.remove('new-menu');
-        elem2.classList.add('new-menu');
-        elem2.classList.remove('no-menu');
-
-    }
