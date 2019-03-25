a3f32acac01f54977d3c012796119e140e8f1c51
teaguru <teaguru@bk.ru>
2019-03-25 16:52:49 +0400

    на одном крыле, но оно взлетело! Юху

--- js/modules/common/menu.js
+++ js/modules/common/menu.js
@@ -1,13 +1,26 @@
+//variables
     var elem = document.querySelector(".logo-item");
     var elem2 = document.querySelector(".logo-menu");
-    var block = document.querySelectorAll(".dropdown-child");
-        elem.onclick = function(){
+    var block = document.querySelectorAll(".header__item");
+//declaretion of visibility
+    for(var i in Object.keys(block))  (block[i].classList.add('no-menu'));
+    elem.classList.add('no-menu');
+           elem2.onclick = function(){
         for(var i in Object.keys(block))  (block[i].classList.add('new-menu'));
-        console.log(block);
+            for(var i in Object.keys(block))  (block[i].classList.remove('no-menu'));
+               elem2.classList.add('no-menu');
+               elem2.classList.remove('new-menu');
+               elem.classList.add('new-menu');
+               elem.classList.remove('no-menu');
 
     }
 
-    elem2.onclick = function() {
-        for (var i in Object.keys(block))  (block[i].classList.remove('new-menu'));
-        console.log(block);
+    elem.onclick = function(){
+        for(var i in Object.keys(block))  (block[i].classList.add('no-menu'));
+        for(var i in Object.keys(block))  (block[i].classList.remove('new-menu'));
+        elem.classList.add('no-menu');
+        elem.classList.remove('new-menu');
+        elem2.classList.add('new-menu');
+        elem2.classList.remove('no-menu');
+
     }
